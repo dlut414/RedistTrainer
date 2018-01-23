810ea62006e93e2036db44c71d775e8b5ba2c493
dlut414 <hfy8373@gmail.com>
2018-01-24 00:08:48 +0900

    trying to use tf

--- RedistTrainer.py
+++ RedistTrainer.py
@@ -4,11 +4,14 @@ sess = tf.InteractiveSession()
 layers = [16, 8, 8, 2]
 
 x = tf.placeholder(tf.float32, shape=[None, layers[0]])
-y = tf.placeholder(tf.float32, shape=[None, layers[-1]])
+y_ = tf.placeholder(tf.float32, shape=[None, layers[-1]])
 
-w = tf.Variable(tf.random_normal(layers))
-b = tf.Variable(tf.zeros(layers[1:]))
+w, b = [], []
+for i in range(1, len(layers)):
+    shape = (layers[i], layers[i-1])
+    w.append(tf.Variable(tf.random_normal(shape, stddev=0.1)))
+    b.append(tf.Variable(tf.zeros(layers[i])))
 
 sess.run(tf.global_variables_initializer())
 
-
+y = tf.Variable(tf.zeros(layers[-1]))
