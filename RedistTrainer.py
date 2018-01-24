b431b8bf62ced76fd04461aafe4c7bfe0c3b03b0
dlut414 <hfy8373@gmail.com>
2018-01-24 21:08:44 +0900

    added train

--- RedistTrainer.py
+++ RedistTrainer.py
@@ -24,9 +24,11 @@ np.random.shuffle(xy)
 x_data = xy[:,:-2]
 y_data = xy[:,-2:]
 
-sess = tf.InteractiveSession()
-
 layers = [16, 8, 8, 2]
+maxIter = 5000
+alpha = 0.03
+reg = 0.0
+batch_size = 200
 
 x = tf.placeholder(tf.float32, shape=[None, layers[0]])
 y_ = tf.placeholder(tf.float32, shape=[None, layers[-1]])
@@ -38,15 +40,27 @@ for i in range(1, len(layers)):
     b.append(tf.Variable(tf.zeros(layers[i])))
     a.append(tf.Variable(tf.zeros(layers[i])))
 
-sess.run(tf.global_variables_initializer())
-
 y = tf.Variable(tf.zeros(layers[-1]))
 for in range(1, len(layers)):
     a[i] = tf.nn.relu(tf.matmul(x, w) + b)
 y = a[-1]
 
 cost = tf.norm(y - y_, ord=2)
-train = tf.train.GradientDescentOptimizer(0.1).minimize(cost)
+train = tf.train.GradientDescentOptimizer(alpha).minimize(cost)
 
 #start training
-
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    n = y_data.shape[0]
+    n_train = int(0.7* n)
+    n_batch = n_train / batch_size
+    x_train = x_data[:n_train,:]
+    y_train = y_data[:n_train,:]
+    x_val = x_data[n_train:,:]
+    y_val = y_data[n_train:,:]
+    for epoch in range(maxIter):
+        for i in range(n_batch):
+            start = i* batch_size
+            stop = np.minimum(start+batch_size, n_train)
+            train.run(feed_dict={x:x_train[start:stop,:], y_:y_train[start:stop,:]})
+        print("cost: ", cost)
