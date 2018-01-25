b14441c360c3c7e3ddd3409f0b6d0f7032e4cd82
dlut414 <hfy8373@gmail.com>
2018-01-25 22:56:09 +0900

    trying to save/restore model

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import numpy as np
+x = tf.get_variable("x", shape=[1, 48])
+y = tf.get_variable("y", shape=[1, 2])
 saver = tf.train.Saver()
-x = tf.get_variable("x")
-y = tf.get_variable("y")
 xx = np.ones((1,48))
 with tf.Session() as sess:
     saver.restore(sess, "./model.ckpt")
